9da9069e30fe8a3c44fe28b691dc969f24c69ef5
Seth <sethyuan@gmail.com>
2022-03-07 22:34:09 +0800

    feat: complete planned functionalities.

--- README.md
+++ README.md
@@ -7,3 +7,34 @@ Ordered lists, flat or nested, multiple formats ordered lists.
 ## 使用展示 (Usage)
 
 ![demo](./demo.gif)
+
+在想要展示为有序列表的块上添加一个以 `#.ol` 开头的标签就可以了。有多种样式可供选择。
+
+Add a tag starting with `#.ol` to display a block as an ordered list. There are multiple formats to choose from.
+
+## 序号样式 (List Styles)
+
+单级列表 (Single-Level List)
+
+- `#.ol`
+- `#.ol-circle`
+- `#.ol-paren`
+- `#.ol-right-paren`
+- `#.ol-dun`
+
+嵌套列表 (Nested List)
+
+- `#.ol-nested`
+- `#.ol-nested-right-paren`
+
+有序列表中的无序列表 (Unordered List in a Ordered List)
+
+- `#.ul`
+
+## 已知问题 (Known Issues)
+
+- 由于是纯 CSS 解决方案，所以在聚焦进有序列表中的块时，会导致 CSS 样式链断裂，有序列表不能被正确渲染。
+- 嵌套列表的嵌套计数器是连续的，也就是说，如果嵌套列表中有单级列表，而单级列表中又有一个嵌套列表的话，这个嵌套列表的层级是从最外层的嵌套列表开始算起的。
+
+- Since it is a pure CSS solution, focusing into blocks in an ordered list causes the CSS style chain to break and the ordered list is not rendered correctly.
+- Nested lists have a continuous nesting counter, meaning that if there is a single-level list in a nested list and a nested list in this single-level list, the levels of this nested list is counted from the outermost nested list.
--- public/index.js
+++ public/index.js
@@ -1,6 +1,10 @@
 import "@logseq/libs"
 
+const Tag = /(^|\s)#\.ol\S*/g
+
 async function main() {
+  const { preferredLanguage: lang } = await logseq.App.getUserConfigs()
+
   logseq.provideStyle(`
     a[data-ref|='.ol'] {
       display: none !important;
@@ -11,11 +15,22 @@ async function main() {
     .ls-block[data-refs-self*='".ol'] .block-children {
       counter-reset: kef-ol;
     }
+    .ls-block[data-refs-self*='".ol-nested'] .block-children {
+      counter-reset: kef-ol kef-ol-nested;
+    }
 
-    .ls-block[data-refs-self*='".ol'] .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a:hover > .bullet-container {
+      background-color: transparent;
+    }
+    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container {
+      min-width: 20px;
+      width: auto;
+      height: auto;
+    }
+    .ls-block[data-refs-self*='".ol'] .ls-block > div > div > a + a > .bullet-container::before {
       display: none;
     }
-    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
       display: block;
       counter-increment: kef-ol;
       content: counter(kef-ol) ". ";
@@ -23,43 +38,97 @@ async function main() {
       font-size: 0.85em;
       min-width: max-content;
     }
-    .ls-block[data-refs-self*='".ol'] .bullet-container {
-      display: flex;
+    .ls-block[data-refs-self*='".ol'] .bullet-container > .bullet {
+      display: block;
     }
-    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container {
+    .ls-block[data-refs-self*='".ol'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container > .bullet {
       display: none;
     }
 
-    .ls-block[data-refs-self*='".ol-nested'] .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ol-nested'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
+      counter-increment: kef-ol-nested;
+      content: counters(kef-ol-nested, ".") ". ";
+    }
+    .ls-block[data-refs-self*='".ol-nested'] .ls-block > div > div > a + a:hover > .bullet-container {
+      background-color: transparent;
+    }
+    .ls-block[data-refs-self*='".ol-nested'] .ls-block > div > div > a + a > .bullet-container {
+      min-width: 20px;
+      width: auto;
+      height: auto;
+    }
+    .ls-block[data-refs-self*='".ol-nested'] .ls-block > div > div > a + a > .bullet-container::before {
       display: block;
-      counter-increment: kef-ol;
-      content: counters(kef-ol, ".") ". ";
+      counter-increment: kef-ol-nested;
+      content: counters(kef-ol-nested, ".") ". ";
       font-family: "Tahoma", sans-serif;
       font-size: 0.85em;
       min-width: max-content;
     }
-    .ls-block[data-refs-self*='".ol-nested'] .bullet-container {
+    .ls-block[data-refs-self*='".ol-nested'] .bullet-container > .bullet {
       display: none;
     }
 
-    .ls-block[data-refs-self*='".ul'] .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ul'] > div > div > a + a > .bullet-container > .bullet {
       display: none;
     }
-    .ls-block[data-refs-self*='".ul'] .bullet-container {
+    .ls-block[data-refs-self*='".ul'] .ls-block > div > div > a + a:hover > .bullet-container {
+      background-color: var(--ls-block-bullet-border-color, #ced9e0);
+    }
+    .ls-block[data-refs-self*='".ul'] .ls-block > div > div > a + a > .bullet-container {
+      width: 20px;
+      height: 20px;
+    }
+    .ls-block[data-refs-self*='".ul'] .ls-block > div > div > a + a > .bullet-container::before {
+      display: none;
+    }
+    .ls-block[data-refs-self*='".ul'] .bullet-container > .bullet {
       display: flex;
     }
 
-    .ls-block[data-refs-self*='".ol-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ol-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
       content: "(" counter(kef-ol) ") ";
     }
-    .ls-block[data-refs-self*='".ol-right-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a::before {
+    .ls-block[data-refs-self*='".ol-right-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
       content: counter(kef-ol) ") ";
     }
-    .ls-block[data-refs-self*='".ol-nested-right-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a::before,
-    .ls-block[data-refs-self*='".ol-nested-right-paren'] .ls-block > div > div > a + a::before {
-      content: counters(kef-ol, ".") ") ";
+    .ls-block[data-refs-self*='".ol-dun'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
+      content: counter(kef-ol) "、";
+    }
+    .ls-block[data-refs-self*='".ol-circle'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before {
+      content: counter(kef-ol);
+      border-radius: 50%;
+      border: 1px solid var(--ls-icon-color);
+      width: 21px;
+      height: 21px;
+      text-align: center;
+    }
+    .ls-block[data-refs-self*='".ol-nested-right-paren'] > .block-children-container > .block-children > .ls-block > div > div > a + a > .bullet-container::before,
+    .ls-block[data-refs-self*='".ol-nested-right-paren'] .ls-block > div > div > a + a > .bullet-container::before {
+      content: counters(kef-ol-nested, ".") ") ";
     }
   `)
+
+  logseq.Editor.registerBlockContextMenuItem(
+    lang === "zh-CN" ? "显示为有序列表" : "Show as ordered list",
+    async ({ uuid }) => {
+      const block = await logseq.Editor.getBlock(uuid)
+      await logseq.Editor.updateBlock(
+        uuid,
+        `${block.content.replace(Tag, "").trimStart()} #.ol`,
+      )
+    },
+  )
+  logseq.Editor.registerBlockContextMenuItem(
+    lang === "zh-CN" ? "显示为嵌套有序列表" : "Show as nested ordered list",
+    async ({ uuid }) => {
+      const block = await logseq.Editor.getBlock(uuid)
+      await logseq.Editor.updateBlock(
+        uuid,
+        `${block.content.replace(Tag, "").trimStart()} #.ol-nested`,
+      )
+    },
+  )
 
   console.log("#ol loaded")
 }
